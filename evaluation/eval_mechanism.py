4cab8ac34e64ba4909987ae0c943dca7e1ac068c
Xiangyu-CAS <398766201@163.com>
2018-02-27 15:18:25 +0800

    eval_mechanism

--- evaluation/eval_mechanism.py
+++ evaluation/eval_mechanism.py
@@ -66,202 +66,6 @@ def normalize(origin_img):
     return origin_img
 
 
-def apply_model(oriImg, model, multiplier):
-    height, width, _ = oriImg.shape
-    #normed_img = normalize(oriImg)
-    normed_img = np.array(oriImg, dtype=np.float32)
-    heatmap_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 19),dtype=np.float32)
-    paf_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 38),dtype=np.float32)
-    stride = 8
-
-    for m in range(len(multiplier)):
-        scale = multiplier[m]
-        imageToTest = cv2.resize(normed_img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-        imgToTest_padded, pad = padRightDownCorner(imageToTest, stride, 128)
-
-        input_img = np.transpose(np.float32(imgToTest_padded[:, :, :, np.newaxis]),
-                                 (3, 2, 0, 1)) / 256 - 0.5  # required shape (1, c, h, w)
-        mask = np.ones((1, 1, input_img.shape[2] / stride, input_img.shape[3] / stride), dtype=np.float32)
-
-        input_var = torch.autograd.Variable(torch.from_numpy(input_img).cuda())
-        mask_var = torch.autograd.Variable(torch.from_numpy(mask).cuda())
-
-        # get the features
-        vec1, heat1, vec2, heat2, vec3, heat3, vec4, heat4, vec5, heat5, vec6, heat6 = model(input_var, mask_var)
-
-        # get the heatmap
-        heatmap = heat6.data.cpu().numpy()
-        heatmap = np.transpose(np.squeeze(heatmap), (1, 2, 0))  # (h, w, c)
-        heatmap = cv2.resize(heatmap, (0, 0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
-        heatmap = heatmap[:imgToTest_padded.shape[0] - pad[2], :imgToTest_padded.shape[1] - pad[3], :]
-        heatmap = cv2.resize(heatmap, (width, height), interpolation=cv2.INTER_CUBIC)
-        heatmap_avg = heatmap_avg + heatmap / len(multiplier)
-
-        # get the paf
-        paf = vec6.data.cpu().numpy()
-        paf = np.transpose(np.squeeze(paf), (1, 2, 0))  # (h, w, c)
-        paf = cv2.resize(paf, (0, 0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
-        paf = paf[:imgToTest_padded.shape[0] - pad[2], :imgToTest_padded.shape[1] - pad[3], :]
-        paf = cv2.resize(paf, (width, height), interpolation=cv2.INTER_CUBIC)
-        paf_avg = paf_avg + paf / len(multiplier)
-
-    all_peaks = []  # all of the possible points by classes.
-    peak_counter = 0
-
-    for part in range(19 - 1):
-        x_list = []
-        y_list = []
-        map_ori = heatmap_avg[:, :, part]
-        map = gaussian_filter(map_ori, sigma=3)
-
-        map_left = np.zeros(map.shape)
-        map_left[1:, :] = map[:-1, :]
-        map_right = np.zeros(map.shape)
-        map_right[:-1, :] = map[1:, :]
-        map_up = np.zeros(map.shape)
-        map_up[:, 1:] = map[:, :-1]
-        map_down = np.zeros(map.shape)
-        map_down[:, :-1] = map[:, 1:]
-
-        peaks_binary = np.logical_and.reduce(
-            (map >= map_left, map >= map_right, map >= map_up, map >= map_down, map > thre1))
-        peaks = zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0])  # note reverse
-        peaks_with_score = [x + (map_ori[x[1], x[0]],) for x in peaks]
-        id = range(peak_counter, peak_counter + len(peaks))
-        peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
-
-        all_peaks.append(peaks_with_score_and_id)
-        peak_counter += len(peaks)
-
-    connection_all = []
-    special_k = []
-    mid_num = 10
-
-    for k in range(len(mapIdx)):
-        score_mid = paf_avg[:, :, [x - 19 for x in mapIdx[k]]]
-        candA = all_peaks[limbSeq[k][0] - 1]
-        candB = all_peaks[limbSeq[k][1] - 1]
-        nA = len(candA)
-        nB = len(candB)
-        indexA, indexB = limbSeq[k]
-        if (nA != 0 and nB != 0):
-            connection_candidate = []
-            for i in range(nA):
-                for j in range(nB):
-                    vec = np.subtract(candB[j][:2], candA[i][:2])
-                    norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
-                    vec = np.divide(vec, norm)
-
-                    startend = zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
-                                   np.linspace(candA[i][1], candB[j][1], num=mid_num))
-
-                    vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
-                                      for I in range(len(startend))])
-                    vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
-                                      for I in range(len(startend))])
-
-                    score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
-                    #score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(
-                    #    0.5 * oriImg.shape[0] / norm - 1, 0)
-                    score_with_dist_prior = sum(score_midpts) / len(score_midpts)
-
-                    criterion1 = len(np.nonzero(score_midpts > thre2)[0]) > 0.8 * len(score_midpts)
-                    criterion2 = score_with_dist_prior > 0
-                    if criterion1 and criterion2:
-                        connection_candidate.append(
-                            [i, j, score_with_dist_prior, score_with_dist_prior + candA[i][2] + candB[j][2]])
-
-            connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
-            connection = np.zeros((0, 5))
-            for c in range(len(connection_candidate)):
-                i, j, s = connection_candidate[c][0:3]
-                if (i not in connection[:, 3] and j not in connection[:, 4]):
-                    connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]])
-                    if (len(connection) >= min(nA, nB)):
-                        break
-
-            connection_all.append(connection)
-        else:
-            special_k.append(k)
-            connection_all.append([])
-
-    subset = -1 * np.ones((0, 20))
-    candidate = np.array([item for sublist in all_peaks for item in sublist])
-
-    for k in range(len(mapIdx)):
-        if k not in special_k:
-            partAs = connection_all[k][:, 0]
-            partBs = connection_all[k][:, 1]
-            indexA, indexB = np.array(limbSeq[k]) - 1
-
-            for i in range(len(connection_all[k])):  # = 1:size(temp,1)
-                found = 0
-                subset_idx = [-1, -1]
-                for j in range(len(subset)):  # 1:size(subset,1):
-                    if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
-                        subset_idx[found] = j
-                        found += 1
-
-                if found == 1:
-                    j = subset_idx[0]
-                    if (subset[j][indexB] != partBs[i]):
-                        subset[j][indexB] = partBs[i]
-                        subset[j][-1] += 1
-                        subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-                elif found == 2:  # if found 2 and disjoint, merge them
-                    j1, j2 = subset_idx
-                    print "found = 2"
-                    membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0:  # merge
-                        subset[j1][:-2] += (subset[j2][:-2] + 1)
-                        subset[j1][-2:] += subset[j2][-2:]
-                        subset[j1][-2] += connection_all[k][i][2]
-                        subset = np.delete(subset, j2, 0)
-                    else:  # as like found == 1
-                        subset[j1][indexB] = partBs[i]
-                        subset[j1][-1] += 1
-                        subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-
-                # if find no partA in the subset, create a new subset
-                elif not found and k < 17:
-                    row = -1 * np.ones(20)
-                    row[indexA] = partAs[i]
-                    row[indexB] = partBs[i]
-                    row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
-                    subset = np.vstack([subset, row])
-
-    deleteIdx = [];
-    for i in range(len(subset)):
-        if subset[i][-1] < 4 or subset[i][-2] / subset[i][-1] < 0.4:
-            deleteIdx.append(i)
-    subset = np.delete(subset, deleteIdx, axis=0)
-
-    canvas = oriImg.copy()
-    # draw points
-    for i in range(18):
-        for j in range(len(all_peaks[i])):
-            cv2.circle(canvas, all_peaks[i][j][0:2], 4, colors[i], thickness=-1)
-
-    # draw lines
-    for i in range(17):
-        for n in range(len(subset)):
-            index = subset[n][np.array(limbSeq[i]) - 1]
-            if -1 in index:
-                continue
-            cur_canvas = canvas.copy()
-            Y = candidate[index.astype(int), 0]
-            X = candidate[index.astype(int), 1]
-            mX = np.mean(X)
-            mY = np.mean(Y)
-            length = ((X[0] - X[1]) ** 2 + (Y[0] - Y[1]) ** 2) ** 0.5
-            angle = math.degrees(math.atan2(X[0] - X[1], Y[0] - Y[1]))
-            polygon = cv2.ellipse2Poly((int(mY), int(mX)), (int(length / 2), stickwidth), int(angle), 0, 360, 1)
-            cv2.fillConvexPoly(cur_canvas, polygon, colors[i])
-            canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
-
-    return candidate, subset, canvas
-
 
 def mechanism(img, img_anns):
     # -----------------------generate GT-------------------------------------
@@ -310,35 +114,30 @@ def mechanism(img, img_anns):
             else:
                 dic['keypoints'][part][2] = 2
 
-        persons.append(dic)
-    person_centers.append(np.append(person_center, max(img_anns[p]['bbox'][2], img_anns[p]['bbox'][3])))
-    if len(persons) > 0:
-        info = dict()
-        info['info'] = []
-        cnt = 1
-        for person in persons:
-            dic = dict()
-            dic['pos'] = person['objpos']
-            dic['keypoints'] = np.zeros((18, 3)).tolist()
-            dic['scale'] = person['scale']
-            for i in range(17):
-                dic['keypoints'][COCO_TO_OURS[i]][0] = person['keypoints'][i][0]
-                dic['keypoints'][COCO_TO_OURS[i]][1] = person['keypoints'][i][1]
-                dic['keypoints'][COCO_TO_OURS[i]][2] = person['keypoints'][i][2]
-            dic['keypoints'][1][0] = (person['keypoints'][5][0] + person['keypoints'][6][0]) * 0.5
-            dic['keypoints'][1][1] = (person['keypoints'][5][1] + person['keypoints'][6][1]) * 0.5
-            if person['keypoints'][5][2] == person['keypoints'][6][2]:
-                dic['keypoints'][1][2] = person['keypoints'][5][2]
-            elif person['keypoints'][5][2] == 2 or person['keypoints'][6][2] == 2:
-                dic['keypoints'][1][2] = 2
-            else:
-                dic['keypoints'][1][2] = 0
-            info['info'].append(dic)
-        lists.append(info)
+        transform_dict = dict()
+        transform_dict['keypoints'] = np.zeros((18, 3)).tolist()
+        for i in range(17):
+            transform_dict['keypoints'][COCO_TO_OURS[i]][0] = dic['keypoints'][i][0]
+            transform_dict['keypoints'][COCO_TO_OURS[i]][1] = dic['keypoints'][i][1]
+            transform_dict['keypoints'][COCO_TO_OURS[i]][2] = dic['keypoints'][i][2]
+        transform_dict['keypoints'][1][0] = (dic['keypoints'][5][0] + dic['keypoints'][6][0]) * 0.5
+        transform_dict['keypoints'][1][1] = (dic['keypoints'][5][1] + dic['keypoints'][6][1]) * 0.5
+
+        if dic['keypoints'][5][2] == dic['keypoints'][6][2]:
+            transform_dict['keypoints'][1][2] = dic['keypoints'][5][2]
+        elif dic['keypoints'][5][2] == 2 or dic['keypoints'][6][2] == 2:
+            transform_dict['keypoints'][1][2] = 2
+        else:
+            transform_dict['keypoints'][1][2] = 0
+
+        persons.append(transform_dict)
 
     kpt = []
-    for x in lists:
-        kpt.append(x['keypoints'])
+    for person in persons:
+        kpt.append(person['keypoints'])
+
+    if len(kpt) == 0:
+        return [],[],img.copy()
 
     import coco_loader
     stride = 8
@@ -353,7 +152,8 @@ def mechanism(img, img_anns):
     cnt = np.zeros((height / stride, width / stride, len(vec_pair[0])), dtype=np.int32)
     vecmap = coco_loader.generate_vector(vecmap, cnt, kpt, vec_pair, stride, theta)
 
-
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_CUBIC)
+    vecmap = cv2.resize(vecmap, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_CUBIC)
     # ---------------post processing----------------------------------
     all_peaks = []  # all of the possible points by classes.
     peak_counter = 0
@@ -518,19 +318,20 @@ def main():
 
     model = pose_estimation.PoseModel(num_point=19, num_vector=19)
 
-    img_dir = '/home/xiangyuzhu/workspace/data/coco/images/val2014/'
-    annFile = '/home/xiangyuzhu/workspace/data/coco/annotations/person_keypoints_minival2014.json'
-    num_imgs = 50 #
+    img_dir = '/home/bst2017/workspace/data/coco/images/val2017/'
+    annFile = '/home/bst2017/workspace/data/coco/annotations/person_keypoints_val2017.json'
+    num_imgs = 50 # COCO 38%
     orderCOCO = [0, -1, 6, 8, 10, 5, 7, 9,  12, 14, 16, 11, 13, 15, 2, 1, 4, 3] #[1, 0, 7, 9, 11, 6, 8, 10, 13, 15, 17, 12, 14, 16, 3, 2, 5, 4]
     myjsonValidate = list(dict())
 
     cocoGt = COCO(annFile)
     img_names = cocoGt.imgs
     # filter only person
-    #cats = cocoGt.loadCats(cocoGt.getCatIds())
-    #catIds = cocoGt.getCatIds(catNms=['person'])
-    #imgIds = cocoGt.getImgIds(catIds=catIds)
-    imgIds = cocoGt.getImgIds()
+    cats = cocoGt.loadCats(cocoGt.getCatIds())
+    catIds = cocoGt.getCatIds(catNms=['person'])
+    imgIds = cocoGt.getImgIds(catIds=catIds)
+
+    #ids = list(cocoGt.imgs.keys())
     #--------------------------------------------------------
     #
     for i in range(num_imgs):
@@ -538,12 +339,11 @@ def main():
         img_info = cocoGt.loadImgs(imgIds[i])[0]
         image_id = img_info['id']
         oriImg = cv2.imread(os.path.join(img_dir, img_info['file_name']))
-        img_anns = cocoGt.loadAnns(image_id)
+        ann_ids = cocoGt.getAnnIds(imgIds=image_id)
+        img_anns = cocoGt.loadAnns(ann_ids)
 
         candidate, subset,canvas = mechanism(oriImg, img_anns)
-
-
-        #cv2.imwrite(os.path.join('./result', img_info['file_name']), canvas)
+        cv2.imwrite(os.path.join('./result', img_info['file_name']), canvas)
         for j in range(len(subset)):
             category_id = 1
             keypoints = np.zeros(51)
